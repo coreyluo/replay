4cf00f4adad9b748eae50cbf92b1e1f0f035be65
yunshan <lxw5347@163.com>
2021-11-23 08:05:53 +0800

    回测

--- test/src/test/java/com/bazinga/test/MainTest.java
+++ test/src/test/java/com/bazinga/test/MainTest.java
@@ -18,7 +18,7 @@ public class MainTest {
 
     public static void main(String[] args) {
 
-        try {
+       /* try {
             String token = JoinQuantUtil.getToken();
             System.out.println(token);
 
@@ -31,6 +31,12 @@ public class MainTest {
             }
         } catch (IOException e) {
             e.printStackTrace();
-        }
+        }*/
+
+        List<String > huiceList = Lists.newArrayList("600556","603178","603002","603327","600576","603916","605018","000810","002045","002468","002164","002467","002060","002992","002120","600580","603081","600688","000861","600860","600246","600012","002488","600892","603489");
+        List<String > onlieList = Lists.newArrayList("600556","000810","002992","600580","002164","002120","603213","002045","603178","600860","002060","605018","600246","000861","002468","600688","002488","002467","603916","603081","603048","600892","603002","600012","603327","603489","600576");
+        System.out.println(onlieList.size());
+        huiceList.removeAll(onlieList);
+        System.out.println(JSONObject.toJSONString(huiceList));
     }
 }
