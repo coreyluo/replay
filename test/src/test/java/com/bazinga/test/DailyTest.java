7ccef410519db6b28197f4f130187e3abe2d006d
yunshan <lxw5347@163.com>
2022-01-27 19:33:54 +0800

    回测

--- test/src/test/java/com/bazinga/test/DailyTest.java
+++ test/src/test/java/com/bazinga/test/DailyTest.java
@@ -14,7 +14,7 @@ public class DailyTest extends BaseTestCase{
         accountPositionCalComponent.cal("398000086400");
         accountPositionCalComponent.cal("398000102550");
         accountPositionCalComponent.cal("398000104348");
-       // accountPositionCalComponent.cal("398000103912");
+        accountPositionCalComponent.cal("398000103912");
         accountPositionCalComponent.cal("398000131333");
         accountPositionCalComponent.cal("398000104352");
     }
