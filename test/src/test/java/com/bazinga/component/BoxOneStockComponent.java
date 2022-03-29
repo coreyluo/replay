499988d1bc7054fc2c4072ea84bfd509d1dd76a9
hubingliang <511632242@qq.com>
2022-03-29 15:06:18 +0800

    新股更新

--- test/src/test/java/com/bazinga/component/BoxOneStockComponent.java
+++ test/src/test/java/com/bazinga/component/BoxOneStockComponent.java
@@ -148,10 +148,10 @@ public class BoxOneStockComponent {
                 List<StockKbar> stockKbars = stockKbarService.listByCondition(query);
                 StockKbar preStockKbar = null;
                 for (StockKbar stockKbar : stockKbars) {
-                    if (DateUtil.parseDate(stockKbar.getKbarDate(), DateUtil.yyyyMMdd).before(DateUtil.parseDate("20200701", DateUtil.yyyyMMdd))) {
+                    if (DateUtil.parseDate(stockKbar.getKbarDate(), DateUtil.yyyyMMdd).before(DateUtil.parseDate("20201001", DateUtil.yyyyMMdd))) {
                         continue;
                     }
-                    if (DateUtil.parseDate(stockKbar.getKbarDate(), DateUtil.yyyyMMdd).after(DateUtil.parseDate("20201001", DateUtil.yyyyMMdd))) {
+                    if (DateUtil.parseDate(stockKbar.getKbarDate(), DateUtil.yyyyMMdd).after(DateUtil.parseDate("20210101", DateUtil.yyyyMMdd))) {
                         continue;
                     }
                 /*if(stockKbar.getKbarDate().equals("20220322")){
